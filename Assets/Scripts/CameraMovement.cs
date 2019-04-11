eae30fb93d6de18c1094355d4b9fdeb2fbec39eb
Bukach Maksim <it.vimax@gmail.com>
2019-04-11 11:00:45 +0300

    Added camera

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	public Transform Target;
+
+	private Vector3 startDistance, moveVec;
+	
+
+	// Use this for initialization
+	void Start ()
+	{
+		startDistance = transform.position - Target.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		moveVec = Target.position + startDistance;
+
+		moveVec.z = 0;
+		moveVec.y = startDistance.y;
+		
+		transform.position = moveVec;
+	}
+}
